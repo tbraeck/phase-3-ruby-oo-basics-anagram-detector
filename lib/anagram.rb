6360af24820f390f9af401cccb13f45844b2cfc6
Tate <tatebraeckel@gmail.com>
2023-03-28 13:27:09 -0500

    commit tree 7573617720e10b7c70a8b16290525c4c48617ebe

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require "pry" 
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+# def match([])
+# end
+
+end
+
+listen = Anagram.new("listen")
+# binding.pry
